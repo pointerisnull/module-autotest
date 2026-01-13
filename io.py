edf1920fe7c13d445801111303f61799bf52a174
Bren <92599085+pointerisnull@users.noreply.github.com>
2026-01-13 11:02:39 -0500

    Update io.py

--- io.py
+++ io.py
@@ -1,14 +1,25 @@
 import RPi.GPIO as GPIO
 
-
 class IOModule():
+    # mode : "BCM" (GPIO #) or "BOARD" (Physical Pin #)
+    def __init__(self, mode=GPIO.BCM):
+        self.mode = mode
+        GPIO.setmode(self.mode)
+    
+    # io_mode : input by default 
+    def setup_pins(pins, io_mode=GPIO.IN):
+        for i in len(pins):
+            GPIO.setup(pins[i], io_mode)
 
     def read_pin(self, pin):
-        pass
+        return GPIO.input(pin)
 
-    def set_pin_d(self, pin, val):
-        pass
+    def set_pin(self, pin, val):
+        GPIO.output(pin, val)
 
 if __name__ == "main":
     io = IOModule()
-    val = io.read_pin(3)
+    io.setup_pins([15, 16], GPIO.OUT)
+    io.setup_pins([28, 29], GPIO.IN)
+    val = io.read_pin(29)
+    print(val)
