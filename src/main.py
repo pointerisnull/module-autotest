3f617e28b50cadab1fe1c0b8e5af212f7a64bdb8
jzink-CE <jzink@ycp.edu>
2026-01-14 12:01:18 -0500

    MORE TEST CHANGES

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 from core.socketHandler import SocketHandler
-from core.tagHandler import tagHandler
-from objects.FlexEdge import FlexEdge
 
-IP = '10.80.14.215' #change based on host
+IP = '10.80.14.215' #change based on remote host
 
 def main():
     socket_handler = SocketHandler(IP, 789)  
@@ -10,8 +8,6 @@ def main():
     socket_handler.open_socket()
 
     socket_handler.close_socket()
-
-
 
 if (__name__ == "__main__"):
     main()
