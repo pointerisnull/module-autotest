93d73b8947d78fa96ec83a24ee1e1f36a4522b33
Bren <92599085+pointerisnull@users.noreply.github.com>
2026-01-14 15:01:13 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,18 +4,19 @@ from objects.Tag import Tag
 import sys
 
 IP = '10.80.14.101' #change based on host
-PORT = 23 # TCP Port
+PORT = 789 # TCP Port
 
 def main():
     socket_handler = SocketHandler(IP, PORT)
     tag_handler = tagHandler(socket_handler)
     tag = Tag("di_1", 0)
-
+    '''
     try:
         socket_handler.open_socket()
     except Exception as e:
         print(e)
         sys.exit()
+    '''
 
     print(tag_handler.get_tag_value(tag))
 
