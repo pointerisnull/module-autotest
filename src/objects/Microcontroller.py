45399f1b7239d466445700d34568b3df92562539
Bren <92599085+pointerisnull@users.noreply.github.com>
2026-01-14 13:44:08 -0500

    pinout

--- src/objects/Microcontroller.py
+++ src/objects/Microcontroller.py
@@ -7,19 +7,19 @@ PINOUT_SETTINGS_PATH = "./settings/pinout.csv"
 
 class RaspberryPi:
     def __init__(self):
-        self.pin_config = self.get_pinout()
-        self.io = IOModule(self.pin_config['MODE'])
+        self.pins = self.get_pinout()
+        self.io = IOModule(self.pins['MODE'])
         # pi output -> module input
-        self.io.setup_pins(self.pin_config['di_x'], GPIO.OUT)
+        self.io.setup_pins(self.pins['di_x'], GPIO.OUT)
         # pi input <- module output
-        self.io.setup_pins(self.pin_config['do_x'], GPIO.IN)
+        self.io.setup_pins(self.pins['do_x'], GPIO.IN)
         # setup address pins, will always be output
-        self.io.setup_pins(self.pin_config['di_a'], GPIO.OUT)
-        self.io.setup_pins(self.pin_config['di_b'], GPIO.OUT)
-        self.io.setup_pins(self.pin_config['di_c'], GPIO.OUT)
-        self.io.setup_pins(self.pin_config['do_a'], GPIO.OUT)
-        self.io.setup_pins(self.pin_config['do_b'], GPIO.OUT)
-        self.io.setup_pins(self.pin_config['do_c'], GPIO.OUT)
+        self.io.setup_pins(self.pins['di_a'], GPIO.OUT)
+        self.io.setup_pins(self.pins['di_b'], GPIO.OUT)
+        self.io.setup_pins(self.pins['di_c'], GPIO.OUT)
+        self.io.setup_pins(self.pins['do_a'], GPIO.OUT)
+        self.io.setup_pins(self.pins['do_b'], GPIO.OUT)
+        self.io.setup_pins(self.pins['do_c'], GPIO.OUT)
     
     def get_pinout(self):
        # read pinout.csv
@@ -49,14 +49,27 @@ class RaspberryPi:
         return [int(bit) for bit in f'{addr:03b}']
 
     # via demux common pin
-    def set_digital_input(self, addr, val=GPIO.IN):
+    def set_digital_input(self, addr, val=GPIO.HIGH):
         bin_addr = self.get_binary_address(addr)
-        input_pin = self.pin_config['di_x']
+        input_pin = self.pins['di_x']
+
+        self.io.set_pin(self.pins['di_a'], bin_addr[0])
+        self.io.set_pin(self.pins['di_b'], bin_addr[1])
+        self.io.set_pin(self.pins['di_c'], bin_addr[2])
+
+        self.io.set_pin(input_pin, val)
 
     # via mux common pin
     def get_digital_output(self, addr):
         bin_addr = self.get_binary_address(addr)
-        output_pin = self.pin_config['do_x']
+        output_pin = self.pins['do_x']
+        
+        self.io.set_pin(self.pins['di_a'], bin_addr[0])
+        self.io.set_pin(self.pins['di_b'], bin_addr[1])
+        self.io.set_pin(self.pins['di_c'], bin_addr[2])
+
+        val = self.io.read_pin(output_pin)
+        return val
 
 class IOModule:
     # mode : "BCM" (GPIO #) or "BOARD" (Physical Pin #)
@@ -85,15 +98,12 @@ if __name__ == "__main__":
     rpi = RaspberryPi()
     print(rpi.get_pinout())
     print(rpi.get_binary_address(3))
-    rpi.set_digital_input(1, 1)
     
-    #io = IOModule()
-    #io.setup_pins(di_1, GPIO.OUT)
-    #io.setup_pins(do_1, GPIO.IN)
+    rpi.set_digital_input(1, 1)
 
     while(1):
-        #io.set_pin(di_1, GPIO.HIGH)
+        rpi.set_digital_input(1, 1)
         time.sleep(1)
-        #io.set_pin(di_1, GPIO.LOW)
+        rpi.set_digital_input(1, 0)
         time.sleep(1)
-        #print(f"Pin {do_1}: {io.read_pin(do_1)}")
+        print(f"Output Pin: {rpi.get_digital_output(1)}")
