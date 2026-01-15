4dd60e4965b3cdbfaa8dc15a9531547bbeb985c3
Bren <92599085+pointerisnull@users.noreply.github.com>
2026-01-15 10:15:38 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,46 @@
 from objects.Microcontroller import RaspberryPi
 from objects.Device import Device
 import utils.FileIO as FileIO
+import time
 
 CONFIG_PATH = "./settings/device_config.csv"
 
+def validate(inpt, crimson_in, crimson_out, out):
+    if inpt == crimson_in == crimson_out == out:
+        return True
+    return False
+
 # simple test to demo since we don't have any hardware
-def demo_test(rpi, flexedge):
-    rpi.set_digital_input(addr=1, val=1)
+def simple_test(rpi, flexedge):
+    true_signal = 1
+    passc = 0
+    try:
+        for i in 1000:
+            rpi.set_digital_input(addr=1, val=true_signal)
+            time.sleep(0.1)
+            input_tag_val = flexedge.get_tag("di_1")
+            output_tag_val = flexedge.get_tag("do_1")
+            time.sleep(0.1)
+            true_output = rpi.read_digital_output(addr=1)
+            
+            print(f"Itteration {i}")
+            print(f"Actual Input: {true_signal}")
+            print(f"Crimson Input: {input_tag_val}")
+            print(f"Crimson Output: {output_tag_val}")
+            print(f"Actual Output: {true_output}")
+
+            if validate():
+                passc += 1
+                print("Test Passed\n")
+            else:
+                print("Test Failed.")
+
+        true_siganl = not true_signal
+
+    except Exception as e:
+        print(f"Test Terminated. Reason: {e}")
     
-    print(flexedge.get_tag("di_1"))
-    print(flexedge.get_tag("do_1"))
+    print(f"Test Completed. Itterations validated: {passc}")
 
 def main():
     rpi = RaspberryPi()
@@ -18,7 +49,7 @@ def main():
     PORT = FileIO.read_csv_setting(CONFIG_PATH, "TCP Port") 
     flexedge = Device(IP, PORT)
 
-    demo_test(rpi, flexedge)
+    simple_test(rpi, flexedge)
 
 if (__name__ == "__main__"):
     main()
