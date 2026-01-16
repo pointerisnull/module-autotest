b936fa0f6351a402076147c0d4ddacea882efb44
Bren <92599085+pointerisnull@users.noreply.github.com>
2026-01-16 14:57:01 -0500

    constants

--- src/common/constants.py
+++ src/common/constants.py
@@ -1,7 +1,13 @@
+# TCP and Crimson download ports
 DEFAULT_PORT = 23
 DEFAULT_DOWNLOAD_PORT = 789
 
+# Crimson user auth
 CRIMSON_USER = "admin"
 CRIMSION_PASS = "password"
 
 BACKOFF_BASE = 2
+
+# File paths
+PINOUT_SETTINGS_PATH = "./settings/pinout.csv"
+CONFIG_PATH = "./settings/device_config.csv"
