93d3f65381eea4d688ea3091c41d1575002d733a
Bren <92599085+pointerisnull@users.noreply.github.com>
2026-01-21 14:20:06 -0500

    Update ThermoTables.py

--- src/common/ThermoTables.py
+++ src/common/ThermoTables.py
@@ -6,6 +6,8 @@ class ThermoTable:
         fpath = "./src/tables/tc_"+THERMO_TYPE
         self.dict = self.load_lookup_table(fpath)
         self.type = THERMO_TYPE
+        self.max_temp = max(self.dict)
+        self.min_temp = min(self.dict)
 
     def load_lookup_table(self, file_path):
         lookup_table = {}
@@ -33,41 +35,39 @@ class ThermoTable:
         if isinstance(val, (float, int)):
             return val
         raise ValueError(f"Temperature {temperature} °C not found in lookup table for Thermocouple Type {self.type}")
-        
 
     def temperature(self, voltage):
-        max_temp = max(self.dict)
-        min_temp = min(self.dict)
+        max_temp = self.max_temp
+        min_temp = self.min_temp
         range = max_temp - min_temp
 
-        temp = None
+        temperature = None
         done = False
         while not done:
             midpoint = round(max_temp - range/2)
-            temp = midpoint
-            print(f"Max: {max_temp}")
-            print(f"Min: {min_temp}")
-            print(f"Mid: {midpoint}")
-            val = self.dict.get(midpoint)
-            print(f"Voltage: {val}\n")
-            if val == voltage:
+            temperature = midpoint
+            #print(f"Max: {max_temp}")
+            #print(f"Min: {min_temp}")
+            #print(f"Mid: {midpoint}")
+            midpoint_voltage = self.dict.get(midpoint)
+            #print(f"Voltage: {midpoint_voltage}\n")
+            if midpoint_voltage == voltage:
                 done = True
-            elif val <= voltage:
-                min_temp = round(max_temp - range/2)
+            elif voltage < midpoint_voltage:
+                max_temp = round(max_temp - range/2)
             else:
-                max_temp = round(min_temp + range/2)
+                min_temp = round(min_temp + range/2)
             range = max_temp - min_temp
             if range <= 1:
                 done = True
         
-        if temp == None:
+        if temperature == None:
             raise ValueError(f"There was a problem performing a reverse lookup on the thermocouple type {self.type} dictionary")
-        return temp           
-
+        return temperature
 
 if __name__ == "__main__":
     tt = ThermoTable("e")
     temp = -30
-    voltage = 5.580
+    voltage = 0.058
     print(f"At {temp} °C the thermocouple has {tt.voltage(temp)} mV across it")
     print(f"At {voltage} mV the thermocouple is {tt.temperature(voltage)} °C")
